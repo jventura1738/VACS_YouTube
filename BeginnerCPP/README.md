9333a2f276157d87123caad68cb1decdaf465b94
Justin Ventura <justinventura@Justins-MacBook-Pro-2.local>
2020-12-22 03:16:05 -0500

    prepare for YT vid

---
+++ BeginnerCPP/README.md
@@ -0,0 +1,7 @@
+# Beginner C++ Series Directory!
+
+by Justin Ventura, University Undergraduate
+
+## Directions:
+
+- beginnerSeries: contains files from the YouTube videos!
---
+++ BeginnerCPP/beginnerSeries/HelloWorld.cpp
@@ -0,0 +1,19 @@
+// This is a comment!
+
+#include <iostream>  // This allows us to use cout, endl.
+
+using namespace std;  // Use this to drop std:: before cout.
+
+// This is the 'main function' which will run our code!
+int main() {
+  /*
+  * This is a block comment!
+  *
+  * Below we will print 'Hello World!' to the terminal
+  * upon the execution of this progam.
+  */
+
+  cout << "Hello World!" << endl;
+
+  return 0;  // This is not necessary in C++, but some use it.
+}
