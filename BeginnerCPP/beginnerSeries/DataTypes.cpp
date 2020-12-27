6ceda02a6dc67719e70bc1718423f1a7fd7f8cf4
λ Justin Ventura <55901784+jventura1738@users.noreply.github.com>
2020-12-26 22:19:02 -0500

    Create DataTypes.cpp

---
+++ BeginnerCPP/beginnerSeries/DataTypes.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  // Integers:
+  int number = 5;
+  cout << "Here is an integer: " << number << "\n";
+
+  // Floats & Doubles:
+  float pi = 3.14f;
+  double e = 2.71d;
+  cout << "Pi: " << pi << ", e: " << e << "\n";
+  
+  // Characters:
+  char grade = 'A';
+  cout << "Letter grade: " << grade << "\n";
+  
+  // Booleans (bool):
+  bool isPassing = True;
+  cout << "This is a passing grade: " << isPassing << "\n";
+  
+  // Strings (strings of characters!):
+  string myName = "Justin";
+  cout << "My name is: " << myName << "\n";
+
+  return 0;
+
+}
