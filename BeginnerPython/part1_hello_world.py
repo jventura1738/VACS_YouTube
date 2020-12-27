9bf8f98ef77f608fcde979552e5845d0acdd5cc4
j.ventura1738 <justinv9001@aol.com>
2020-12-27 00:35:24 -0500

    update

---
+++ BeginnerPython/part1_hello_world.py
@@ -0,0 +1,10 @@
+# Justin Ventura [12/28/20]
+# Ventura Academy YT Computer Science
+# -> github.com/jventura1738/VACS_YoutTube
+
+print('Hello World!')
+
+my_name = input('What is your name?')
+my_age = input('How old are you?')
+
+print(f'So you are {my_name}, {my_age} years old.  Nice to meet you!')
