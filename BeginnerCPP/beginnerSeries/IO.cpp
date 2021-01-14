7b338d86571eb67e0cbf526bc3fa024636be7b1e
j.ventura1738 <justinv9001@aol.com>
2021-01-13 23:40:43 -0500

    update

--- BeginnerCPP/beginnerSeries/IO.cpp
+++ BeginnerCPP/beginnerSeries/IO.cpp
@@ -12,8 +12,20 @@ int main() {
     cout << "Hey, what is your name?\n";
     cout << "--> ";
     cin >> myName;
-    cout << "Hello " << myName << ", nice to meet you!\n";
+    cout << "Hello " << myName << ", nice to meet you!\n\n";
+
+    string month, day, year;
+    cout << "Enter your birthday as so: Month Day Year...\n";
+    cout << "--> ";
+    cin >> month >> day >> year;
+    cout << "Entered Birthday: " << month << " " << day << " " << year << ".\n\n";
 
     return 0;
-    
 }
+
+/**
+ * ! Don't forget, you can compile this program like so:
+ * ! g++ -c IO.cpp
+ * ! g++ -o IO IO.o
+ * ! ./IO
+*/
